c6a296781f879fa0f04191dc691893e968fbd5f6
kielhong <mhhong@woowahan.com>
2016-06-07 11:02:56 +0900

    DomainClassConverter 사용 추가

--- src/main/java/com/widehouse/web/IndexController.java
+++ src/main/java/com/widehouse/web/IndexController.java
@@ -4,9 +4,12 @@ import com.widehouse.event.Event;
 import com.widehouse.event.EventService;
 import com.widehouse.user.User;
 import com.widehouse.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +21,7 @@ import java.util.stream.Collectors;
  * Created by kiel on 2016. 5. 24..
  */
 @Controller
+@Slf4j
 public class IndexController {
     @Autowired
     private UserRepository userRepository;
@@ -32,6 +36,18 @@ public class IndexController {
 
         model.addAttribute("defaultDate", defaultDate);
         model.addAttribute("events", events.stream().map(EventView::new).collect(Collectors.toList()));
+
+        return "index";
+    }
+
+    @GetMapping("/users/{id}")
+    public String userCalendar(@PathVariable("id") User user, Model model) {
+        LocalDate defaultDate = LocalDate.of(2016, 5, 1);
+        List<Event> events = eventService.listUserEventByMonth(user, defaultDate);
+
+        model.addAttribute("defaultDate", defaultDate);
+        model.addAttribute("events", events.stream().map(EventView::new).collect(Collectors.toList()));
+
 
         return "index";
     }
--- src/test/java/com/widehouse/web/IndexControllerTest.java
+++ src/test/java/com/widehouse/web/IndexControllerTest.java
@@ -1,31 +1,63 @@
 package com.widehouse.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.widehouse.api.ApiEventController;
+import com.widehouse.event.Event;
+import com.widehouse.event.EventService;
+import com.widehouse.user.User;
+import com.widehouse.user.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
- * Created by kiel on 2016. 5. 24..
+ * Created by kiel on 2016. 6. 7..
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(IndexController.class)
 public class IndexControllerTest {
     @Autowired
-    TestRestTemplate template;
-
+    MockMvc mockMvc;
+    @MockBean
+    UserRepository userRepository;
+    @MockBean
+    EventService eventService;
+    @InjectMocks
+    IndexController indexController;
 
     @Test
-    public void testIndex() throws Exception {
-        // When
-        ResponseEntity<String> responseEntity = template.getForEntity("/", String.class);
-        // Then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    public void userCalendar_calendarByUser() throws Exception {
+        Long userId = 1L;
+        User user = new User();
+        given(userRepository.findOne(userId))
+                .willReturn(user);
+        given(eventService.listUserEventByMonth(user, LocalDate.of(2016,5,1)))
+                .willReturn(Arrays.asList(new Event()));
+
+        // Expected
+        mockMvc.perform(get("/users/{id}", userId))
+                .andExpect(status().isInternalServerError())  // DomainClassConverter needs ApplicationContext. MockMvc not use ApplicationContext
+                .andExpect(handler().handlerType(IndexController.class))
+                .andExpect(handler().methodName("userCalendar"))
+                .andDo(print());
+
     }
 }
